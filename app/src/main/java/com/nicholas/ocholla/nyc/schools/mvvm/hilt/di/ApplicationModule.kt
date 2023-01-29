78be3b592045837c36604654a945bd8fa5f27aef
NICHOLAS OCHOLLA <nocholla@yahoo.com>
2023-01-29 14:38:45 -0800

    Added Application Module
    Added Application Module

---
+++ app/src/main/java/com/nicholas/ocholla/nyc/schools/mvvm/hilt/di/ApplicationModule.kt
@@ -0,0 +1,53 @@
+package com.nicholas.ocholla.nyc.schools.mvvm.hilt.di
+
+import com.nicholas.ocholla.nyc.schools.mvvm.hilt.BuildConfig
+import com.nicholas.ocholla.nyc.schools.mvvm.hilt.data.api.ApiHelper
+import com.nicholas.ocholla.nyc.schools.mvvm.hilt.data.api.ApiHelperImpl
+import com.nicholas.ocholla.nyc.schools.mvvm.hilt.data.api.ApiService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+class ApplicationModule {
+
+    @Provides
+    fun provideBaseUrl() = BuildConfig.BASE_URL
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient() = if (BuildConfig.DEBUG) {
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+        OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+    } else OkHttpClient
+        .Builder()
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient, BASE_URL: String): Retrofit =
+        Retrofit.Builder()
+            .addConverterFactory(MoshiConverterFactory.create())
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideApiService(retrofit: Retrofit): ApiService = retrofit.create(ApiService::class.java)
+
+    @Provides
+    @Singleton
+    fun provideApiHelper(apiHelper: ApiHelperImpl): ApiHelper = apiHelper
+
+}
