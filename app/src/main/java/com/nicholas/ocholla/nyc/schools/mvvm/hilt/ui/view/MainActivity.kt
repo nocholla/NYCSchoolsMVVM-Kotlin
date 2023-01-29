05e4632364528b14958cc64423c0384dcdf761c4
NICHOLAS OCHOLLA <nocholla@yahoo.com>
2023-01-29 14:44:56 -0800

    Moved MainActivity to View package
    Moved MainActivity to View package

--- app/src/main/java/com/nicholas/ocholla/nyc/schools/mvvm/hilt/ui/view/MainActivity.kt
+++ app/src/main/java/com/nicholas/ocholla/nyc/schools/mvvm/hilt/ui/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.nicholas.ocholla.nyc.schools.mvvm.hilt
+package com.nicholas.ocholla.nyc.schools.mvvm.hilt.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.nicholas.ocholla.nyc.schools.mvvm.hilt.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
