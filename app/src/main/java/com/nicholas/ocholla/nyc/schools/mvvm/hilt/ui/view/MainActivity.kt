a646ec992a07ca69de76036eb75bc4504e616771
NICHOLAS OCHOLLA <nocholla@yahoo.com>
2023-01-29 17:15:09 -0800

    Updated Main Activity

--- app/src/main/java/com/nicholas/ocholla/nyc/schools/mvvm/hilt/ui/view/MainActivity.kt
+++ app/src/main/java/com/nicholas/ocholla/nyc/schools/mvvm/hilt/ui/view/MainActivity.kt
@@ -3,9 +3,12 @@ package com.nicholas.ocholla.nyc.schools.mvvm.hilt.ui.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.nicholas.ocholla.nyc.schools.mvvm.hilt.R
 import com.nicholas.ocholla.nyc.schools.mvvm.hilt.ui.adapter.SchoolListAdapter
 import com.nicholas.ocholla.nyc.schools.mvvm.hilt.ui.viewmodel.SchoolViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,4 +19,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    private fun setupUI() {
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = SchoolListAdapter(this, arrayListOf())
+        recyclerView.addItemDecoration(
+            DividerItemDecoration(
+                recyclerView.context,
+                (recyclerView.layoutManager as LinearLayoutManager).orientation
+            )
+        )
+        recyclerView.adapter = adapter
+    }
+
 }
