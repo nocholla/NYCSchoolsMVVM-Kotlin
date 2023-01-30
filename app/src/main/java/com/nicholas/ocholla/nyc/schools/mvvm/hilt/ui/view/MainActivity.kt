135cb566b27da2a96cd03e398b61181b09de9fa9
NICHOLAS OCHOLLA <nocholla@yahoo.com>
2023-01-29 17:11:24 -0800

    Updated MainActivity
    Updated MainActivity

--- app/src/main/java/com/nicholas/ocholla/nyc/schools/mvvm/hilt/ui/view/MainActivity.kt
+++ app/src/main/java/com/nicholas/ocholla/nyc/schools/mvvm/hilt/ui/view/MainActivity.kt
@@ -2,9 +2,16 @@ package com.nicholas.ocholla.nyc.schools.mvvm.hilt.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.nicholas.ocholla.nyc.schools.mvvm.hilt.R
+import com.nicholas.ocholla.nyc.schools.mvvm.hilt.ui.adapter.SchoolListAdapter
+import com.nicholas.ocholla.nyc.schools.mvvm.hilt.ui.viewmodel.SchoolViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private val schoolViewModel : SchoolViewModel by viewModels()
+    private lateinit var adapter: SchoolListAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
